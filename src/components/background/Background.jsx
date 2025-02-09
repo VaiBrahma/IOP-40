686f15d5d4349629c6df77e926d87c30be7a5ffd
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2025-02-09 16:17:14 +0530

    background and navbar added

---
+++ src/components/background/Background.jsx
@@ -0,0 +1,14 @@
+import styles from './Background.module.css';
+
+const Background = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.topbar}></div>
+        <div className={`${styles.imgg} centralizeContent`}>
+            <img src='/src/assets/images/circuit.png' className={styles.centreImg}/>
+        </div>
+    </div>
+  );
+};
+
+export default Background;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from 'react';
+import styles from './Navbar.module.css';
+import {FaBars, FaTimes } from "react-icons/fa";
+
+
+const Navbar = () => {
+    const [vis, setVis] = useState(false);
+    const [title, setTitle] = useState("IOP-40: Transient Stability Analysis");
+    const [navHeight, setNavHeight] = useState(120);
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 650) {
+        setTitle("IOP-40: TSA");
+      }else {
+        setTitle("IOP-40: Transient Stability Analysis");
+      }
+    };
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+          setNavHeight(50);
+        } else {
+          setNavHeight(120);
+        }
+    };
+
+    handleResize();
+    handleScroll();
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+        window.removeEventListener("resize", handleResize);
+        window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
+  return (
+
+    <div className={styles.container} style={{height: `${navHeight}px`}}>
+        <div className={styles.iitrLogo}>
+            <a href="https://www.iitr.ac.in"><img src="/src/assets/images/iitrlogo.png" style={{height:"100%"}} alt="O" /></a>
+        </div>
+        <div className={styles.title}>{title}</div>
+        {!vis ? <FaBars size={30} className={styles.hamburger} onClick={()=>{setVis(e=>!e)}}/> : <FaTimes size={30} className={styles.hamburger} onClick={()=>{setVis(e=>!e)}}/> }
+    </div>
+    
+  );
+};
+
+export default Navbar;

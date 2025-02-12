9ba8622ee7da1a5524cab62e493dfba2b56a323b
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2025-02-12 13:26:57 +0530

    homepage created

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,10 +4,31 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to Power System Analysis</h1>
+      <div className={styles.title}>
+        <div>IOP-40</div>
+      </div>
+
       <div className={styles.buttonContainer}>
         <Link to="/fdlf" className={styles.btn}>FDLF</Link>
         <Link to="/tsa" className={styles.btn}>TSA</Link>
+      </div>
+
+      <div className={styles.footer}>
+        <div className={styles.teamName}>
+          <h2>Submitted By: </h2>
+            <li>Palak Tripathi</li>
+            <li>Vaibhav Singh</li>
+            <li>Garv</li>
+            <li>Vaibhav Kumar</li>
+        </div>
+
+        <div className={styles.proff}>
+          <h2>Under the Guidance of: </h2>
+          <p>Dr. Abdul Saleem Mir</p>
+          <div className={styles.role}>Assistant Professor</div>
+          <div>Department of Electrical Engineering</div>
+          <div>Indian Institute of Technology Roorkee</div>
+        </div>
       </div>
     </div>
   );

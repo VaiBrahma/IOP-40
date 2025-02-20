85da94294f0273a38241e1e2eda00fafd173718f
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2025-02-20 19:06:10 +0530

    form added to upload file

--- src/components/fileInput/FileInput.jsx
+++ src/components/fileInput/FileInput.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react';
+import styles from './FileInput.module.css';
 import * as XLSX from 'xlsx';
 
 const FileInput = () => {
+  const [file, setFile] = useState(null);
   const [data, setData] = useState(null);
 
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!file) {
+      alert('Please select a file first');
+      return;
+    }
+
+    const reader = new FileReader();
     reader.onload = (event) => {
       const workbook = XLSX.read(event.target.result, { type: 'binary' });
       const sheetName = workbook.SheetNames[0];
@@ -16,19 +27,19 @@ const FileInput = () => {
 
       setData(sheetData);
     };
-
     reader.readAsBinaryString(file);
   };
 
   return (
     <div>
-      <input type="file" onChange={handleFileUpload} />
-      {data && (
-        <div>
-          <h2>Imported Data:</h2>
-          <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div className={styles.overlay}>
+        <div className={styles.modal}>
+          <form onSubmit={handleSubmit}>
+            <input type="file" onChange={handleFileChange} />
+            <button type="submit" className={`${styles.sub} btn`}>Submit</button>
+          </form>
         </div>
-      )}
+      </div>
     </div>
   );
 }
